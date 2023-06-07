d41a531eb5a8207a06ceb604d83d23ae2f1112d5
tomasbabulak <babulak.tomas@gmail.com>
2023-06-07 20:33:34 +0200

    Initial commit

---
+++ SimpleBeaconApp/SimpleBeaconApp/SimpleBeaconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBeaconAppApp.swift
+//  SimpleBeaconApp
+//
+//  Created by Tomas Babulak on 07.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBeaconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
